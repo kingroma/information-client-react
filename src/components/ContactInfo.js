c2df71952783522e967e5075049be13fac8efb72
tintoll <blue@tigrison.com>
2017-06-14 12:55:57 +0900

    연락처 등록및 선택 이벤트 추가(Immutability Helpers사용)

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
 export default class ContactInfo extends React.Component {
+  
+  handleClick(){
+    this.props.onSelect(this.props.contactKey);
+  }
+
   render(){
+    let getStyle = isSelect => {
+      if(!isSelect) return;
+      let style = {
+        fontWeight : 'bold',
+        backgroundColor : '#4efcd8'
+      }
+      return style;
+    }
     return(
-      <li>{this.props.name} {this.props.phone}</li>
+      <li onClick={this.handleClick.bind(this)}
+          style={getStyle(this.props.isSelected)} >
+          {this.props.name} {this.props.phone}
+      </li>
     );
   }
 }
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ContactInfo from './ContactInfo';
+import ContactCreator from './ContactCreator';
+import update from 'react-addons-update';
 
 export default class Contacts extends React.Component {
 
@@ -11,8 +13,38 @@ export default class Contacts extends React.Component {
         {name:"ales", phone:"010-0000-0001"},
         {name:"Betty", phone:"010-0000-0002"},
         {name:"Chalie", phone:"010-0000-0003"},
-        {name:"Chalie", phone:"010-0000-0004"}
-      ]
+        {name:"dyna", phone:"010-0000-0004"}
+      ], 
+      selectedKey : -1
+    }
+  }
+  _insertContact(name, phone) {
+    let newState = update(this.state,{
+      contactData :{
+        $push : [{"name":name, "phone":phone}]  
+      }      
+    });
+    this.setState(newState);
+  }
+
+  _onSelect(key){
+    if(key == this.state.selectedKey){
+      console.log('key select cancelled');
+      this.setState({
+        selectedKey : -1
+      });
+      return;
+    }
+    this.setState({
+      selectedKey : key
+    });
+    console.log(key + 'is selected');
+  }
+  _isSelected(key){
+    if(this.state.selectedKey == key){
+      return true;
+    }else{
+      return false;
     }
   }
 
@@ -26,8 +58,13 @@ export default class Contacts extends React.Component {
             return (<ContactInfo name={contact.name} 
                                 phone={contact.phone}
                                 key={i}
+                                contactKey={i}
+                                isSelected={this._isSelected.bind(this)(i)}
+                                onSelect={this._onSelect.bind(this)}
                   />);
           })}
+
+          <ContactCreator onInsert={this._insertContact.bind(this)}/>
         </ul>
       </div>
     );
